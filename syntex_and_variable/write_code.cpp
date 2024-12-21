dcee1113f316d743f650148f70974845efdf7b65
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-12-21 18:23:00 +0600

    Add program to demonstrate variable declarations in C++
    - Declared variables of different data types:
      - Integer initialized with 42
      - Floating-point initialized with 3.14
      - Character initialized with 'A'
      - String initialized with Hello, C++!
    - Includes detailed comments for clarity

---
+++ syntex_and_variable/write_code.cpp
@@ -0,0 +1,23 @@
+/*
+1. Variables and Data Types
+    Question: Declare variables of the following types:
+        An integer with a value of 42.
+        A floating-point number with a value of 3.14.
+        A character with the value 'A'.
+        A string with the value "Hello, C++!".
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x = 24;
+    float y = 3.14;
+    char a = 'A';
+    string str = "Hello, C++!";
+    return 0;
+}
