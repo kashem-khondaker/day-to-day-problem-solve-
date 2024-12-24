293f079381dd76ef75a1eb70c71640da20c81d29
kashem-khondaker <kashem.khondaker.official001@gmail.com>
2024-12-24 09:48:29 +0600

    https://codeforces.com/problemset/problem/112/A problem solve

---
+++ Lexicograpical_string_problem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    // Convert both strings to lowercase
+    transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
+    transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+
+    // Compare strings lexicographically
+    if (str1 < str2) {
+        cout << "-1" << endl;
+    } else if (str1 > str2) {
+        cout << "1" << endl;
+    } else {
+        cout << "0" << endl;
+    }
+
+    return 0;
+}
